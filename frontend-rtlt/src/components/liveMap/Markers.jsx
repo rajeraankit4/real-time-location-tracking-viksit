8b408c16d83b87e04e8b4a616d957a7ea8c53887
Ankit Kumar <rajeraankit4@gmail.com>
2025-10-26 21:41:10 +0530

    Added username in popup

--- frontend-rtlt/src/components/liveMap/Markers.jsx
+++ frontend-rtlt/src/components/liveMap/Markers.jsx
@@ -2,12 +2,12 @@
 import React from "react";
 import { Marker, Popup } from "react-leaflet";
 
-export default function Markers({ locations }) {
+export default function Markers({ locations, userName }) {
   return (
     <>
       {Object.entries(locations).map(([id, loc]) => (
         <Marker key={id} position={[loc.lat, loc.lng]}>
-          <Popup>{loc.userName || id}</Popup>
+          <Popup>{userName}</Popup>
         </Marker>
       ))}
     </>
